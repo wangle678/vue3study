5c568a60af6b163ca3dcd36be0878142b4cf0fe9
chuqian <1137659741@qq.com>
2021-06-03 09:59:19 +0800

    页面测导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <div>巴拉巴拉</div>
+  <div id="app">
+    <home></home>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import home from "./page/index"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    home
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/page/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+      <div class="side-navbar">
+          <div class="navbar-title" v-for="item in sideNavbarData" :key="item.id">{{item.bigTitle}}</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    data() {
+        return {
+            sideNavbarData: [
+                {
+                    id: 1,
+                    bigTitle: "基础"
+                },
+                {
+                    id: 2,
+                    bigTitle: "深入组件"
+                },
+                {
+                    id: 3,
+                    bigTitle: "过渡 & 动画"
+                },
+                {
+                    id: 4,
+                    bigTitle: "可复用 & 组合"
+                },
+                {
+                    id: 5,
+                    bigTitle: "高阶指南"
+                },
+                {
+                    id: 6,
+                    bigTitle: "工具"
+                },
+                {
+                    id: 7,
+                    bigTitle: "规模化"
+                },
+                {
+                    id: 8,
+                    bigTitle: "无障碍"
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+body {
+    margin: 0px;
+    padding: 0px;
+}
+.side-navbar {
+    width: 200px;
+    height: 100vh;
+    overflow: auto;
+    padding-left: 10px;
+    background-color: pink;
+}
+.side-navbar .navbar-title {
+    font-size: 20px;
+}
+</style>
